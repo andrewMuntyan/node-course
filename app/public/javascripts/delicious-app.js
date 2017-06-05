1ad4fded57b2d1e9faec94bfef15709edcb9e962
andrew.muntyan <andrew.muntyan@nextiva.com>
2017-06-05 20:55:46 +0300

    geolocation autocomplete

--- app/public/javascripts/delicious-app.js
+++ app/public/javascripts/delicious-app.js
@@ -1,3 +1,6 @@
 import '../sass/style.scss';
 
-// import { $, $$ } from './modules/bling';
+import { $, $$ } from './modules/bling'; // eslint-disable-line
+import autocomplete from './modules/autocomplete';
+
+autocomplete( $('#address'), $('#lat'), $('#lng') );
