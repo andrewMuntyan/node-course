e698bca842855ddabeb73bc6d969c71730174210
andrew.muntyan <andrew.muntyan@nextiva.com>
2017-06-05 11:44:20 +0300

    saving data to db from /add route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+// for processing async errors
+const { catchErrors } = require('../handlers/errorHandlers');
+
 // routes controller
 const storeController = require('../controllers/storeController');
 
 // Do work here
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
