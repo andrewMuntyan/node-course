de0b8b9e7a3597e09f31b187db2fb18d8b8fbcce
Andrew <Muntyan>
2017-06-17 17:27:17 +0300

    searching added

--- app/public/javascripts/delicious-app.js
+++ app/public/javascripts/delicious-app.js
@@ -1,6 +1,10 @@
 import '../sass/style.scss';
 
+import typeAhead from './modules/typeAhead'
+
 import { $, $$ } from './modules/bling'; // eslint-disable-line
 import autocomplete from './modules/autocomplete';
 
 autocomplete( $('#address'), $('#lat'), $('#lng') );
+
+typeAhead($('.search'))
